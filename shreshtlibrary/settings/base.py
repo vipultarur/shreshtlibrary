98417807ae06ea7bc7c906bfc11dcb23dd236bd2
vipultarur <vipultarur2003@gmail.com>
2026-06-19 22:14:20 +0530

    smtp update

--- shreshtlibrary/settings/base.py
+++ shreshtlibrary/settings/base.py
@@ -210,3 +210,11 @@ STORAGES = {
 }
 
 CORS_ALLOW_ALL_ORIGINS = True
+
+# Email Configuration
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
