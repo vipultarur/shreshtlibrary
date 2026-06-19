17b205a78e5ae1cef7eb6ac908e54592fb55a4ff
vipultarur <vipultarur2003@gmail.com>
2026-06-19 22:21:18 +0530

    Configure direct SMTP credentials in base settings

--- shreshtlibrary/settings/base.py
+++ shreshtlibrary/settings/base.py
@@ -216,5 +216,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = 'vipultarur2003@gmail.com'
+EMAIL_HOST_PASSWORD = 'ounn zeso yxdi lsra'
