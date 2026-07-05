861296092e6fc8d06f96e5a7f2a21d591e172df7
vipultarur <vipultarur2003@gmail.com>
2026-07-05 19:00:30 +0530

    chore: change WhatsApp session ID to 'default'

--- Services/AdminBillingService.cs
+++ Services/AdminBillingService.cs
@@ -1138,7 +1138,7 @@ namespace WebApplication1.Services
                     page.Size(PageSizes.A5);
                     page.Margin(30);
                     page.PageColor(Colors.White);
-                    page.DefaultTextStyle(x => x.FontSize(11).FontFamily(Fonts.Arial));
+                    page.DefaultTextStyle(x => x.FontSize(11));
 
                     page.Content().Column(col =>
                     {
